c79be67dc42e376e2b6c0e903d3f241fd745a6f4
Raymond Sun <raymonds1@student.unimelb.edu.au>
2018-04-24 20:15:57 +1000

    read seed from properties, also test commit

--- Part_B/swen30006/automail/Simulation.java
+++ Part_B/swen30006/automail/Simulation.java
@@ -5,6 +5,7 @@ import exceptions.ItemTooHeavyException;
 import exceptions.MailAlreadyDeliveredException;
 import strategies.Automail;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -24,7 +25,7 @@ public class Simulation {
     private static double total_score = 0;
 
     public static void main(String[] args) { //throws IOException {
- /*   	// Should probably be using properties here
+     	// Should probably be using properties here
     	Properties automailProperties = new Properties();
 		// Defaults
 		automailProperties.setProperty("Name_of_Property", "20");  // Property value may need to be converted from a string to the appropriate type
@@ -33,15 +34,18 @@ public class Simulation {
 		
 		try {
 			inStream = new FileReader("automail.properties");
+			
 			automailProperties.load(inStream);
-		} finally {
-			 if (inStream != null) {
-	                inStream.close();
-	            }
-		}
+			
+			if (inStream != null) {
+                inStream.close();
+            }
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
 		
 		int i = Integer.parseInt(automailProperties.getProperty("Name_of_Property"));
-*/
 
         MAIL_DELIVERED = new ArrayList<MailItem>();
                 
@@ -49,12 +53,8 @@ public class Simulation {
         HashMap<Boolean, Integer> seedMap = new HashMap<>();
         
         /** Read the first argument and save it as a seed if it exists */
-        if(args.length != 0){
-        	int seed = Integer.parseInt(args[0]);
-        	seedMap.put(true, seed);
-        } else{
-        	seedMap.put(false, 0);
-        }
+        seedMap.put(true, Integer.parseInt(automailProperties.getProperty("Seed")));
+
         Automail automail = new Automail(new ReportDelivery());
         MailGenerator generator = new MailGenerator(MAIL_TO_CREATE, automail.mailPool, seedMap);
         
