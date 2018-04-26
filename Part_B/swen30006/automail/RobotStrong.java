ba7fc6f9c697c72f4e983df7b5cddccb25e780a3
Chris Chapman <chapmanc1@student.unimelb.edu.au>
2018-04-26 15:35:00 +1000

    Fixed wording to make tube capacity clearer

--- Part_B/swen30006/automail/RobotStrong.java
+++ Part_B/swen30006/automail/RobotStrong.java
@@ -5,9 +5,9 @@ import strategies.IMailPool;
 public class RobotStrong extends Robot {
 
 	public static final boolean IS_STRONG = true;
-	public static final int MAXIMUM_CAPACITY = 4;
+	public static final int MAXIMUM_TUBE_CAPACITY = 4;
 	
 	public RobotStrong(IMailDelivery delivery, IMailPool mailPool) {
-		super(delivery, mailPool, MAXIMUM_CAPACITY, IS_STRONG);
+		super(delivery, mailPool, MAXIMUM_TUBE_CAPACITY, IS_STRONG);
 	}
 }
--- Part_B/swen30006/automail/RobotWeak.java
+++ Part_B/swen30006/automail/RobotWeak.java
@@ -5,9 +5,9 @@ import strategies.IMailPool;
 public class RobotWeak extends Robot {
 
 	public static final boolean IS_STRONG = false;
-	public static final int MAXIMUM_CAPACITY = 4;
+	public static final int MAXIMUM_TUBE_CAPACITY = 4;
 	
 	public RobotWeak(IMailDelivery delivery, IMailPool mailPool) {
-		super(delivery, mailPool, MAXIMUM_CAPACITY, IS_STRONG);
+		super(delivery, mailPool, MAXIMUM_TUBE_CAPACITY, IS_STRONG);
 	}
 }
