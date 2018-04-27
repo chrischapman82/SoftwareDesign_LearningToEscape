3913fe43e07185fe1c584f08803949f8f550d262
Liam Whittle <l.whittle@student.unimelb.edu.au>
2018-04-27 15:41:14 +1000

    implemented variable tube capacity

--- Part_B/swen30006/automail/RobotBig.java
+++ Part_B/swen30006/automail/RobotBig.java
@@ -5,8 +5,9 @@ import strategies.IMailPool;
 public class RobotBig extends Robot {
 
 	public static final boolean IS_STRONG = false;
+	public static final int MAX_TUBE_CAPACITY = 6;
 	
 	public RobotBig(IMailDelivery delivery, IMailPool mailPool) {
-		super(delivery, mailPool, IS_STRONG);
+		super(delivery, mailPool, IS_STRONG, MAX_TUBE_CAPACITY);
 	}
 }
