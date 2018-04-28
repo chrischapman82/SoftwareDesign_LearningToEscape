a12b7a14b55fe911fe34d207960ecc8fcb463882
Chris Chapman <chapmanc1@student.unimelb.edu.au>
2018-04-28 20:24:43 +1000

    Commented IMasterPool

--- Part_B/swen30006/strategies/IMasterPool.java
+++ Part_B/swen30006/strategies/IMasterPool.java
@@ -1,12 +1,15 @@
 package strategies;
-import java.util.ArrayList;
 
 import automail.*;
 
-
+/* IMasterPool controls and regulates the MailPool.
+ * When new mail is generated, distributes mail to the different IMailPools.
+ * For a robot, delegates them a mailPool that a robot will be accessing and putting mail into.
+ */
 public interface IMasterPool {
 	
-	// passes off mail to a mail pool
+	/* Distributes mailItem to one of its mail pools based on mail attributes.
+	 */
 	void distributeMail(MailItem m);
 	
 }
