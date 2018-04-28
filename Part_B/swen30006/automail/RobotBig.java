4d4cfb200d1ac31e76b871749f6b666504043dc6
Chris Chapman <chapmanc1@student.unimelb.edu.au>
2018-04-28 18:08:55 +1000

    committing before merge

--- Part_B/swen30006/automail/RobotBig.java
+++ Part_B/swen30006/automail/RobotBig.java
@@ -9,7 +9,7 @@ import strategies.IMailPool;
  */
 public class RobotBig extends Robot {
 
-	public static final boolean IS_STRONG = false;
+	public static final boolean IS_STRONG = true;
 	public static final int MAX_TUBE_CAPACITY = 6;
 	public static final String TAG = "big";
 	
